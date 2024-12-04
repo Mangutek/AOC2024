372c523a35bbb7893d56c37966df5b5385fa20ab
Franciszek Żółtogórski <152714905+Mangutek@users.noreply.github.com>
2024-12-04 13:58:27 +0100

    Update DAY3.py

--- DAY3.py
+++ DAY3.py
@@ -1,22 +1,21 @@
-import re
-file = open("C:\\Users\\Franek\\Desktop\\AOC\\DAY3\\database3.txt")
+import re  #importuje REGULAR EXPRESION
+file = open("C:\\Users\\Franek\\Desktop\\AOC\\DAY3\\database3.txt") #wczytuje plik
 tekst = file.readlines()
 data = []
 wynik =0
 
-for linia in tekst:
+for linia in tekst:            #wczytuje plik
     data.append(linia)
 
-example = 'mul\([0-9]+,[0-9]+\)'
+example = 'mul\([0-9]+,[0-9]+\)'  #tworze wzór szukanego stringa/ wszystko tu --> https://www.w3schools.com/python/python_regex.asp
 example2 = '[0-9]+'
 
 
-for n in range(len(data)):
+for n in range(len(data)):                #szukam w tekscie mul() 
     dane = re.findall(example, data[n])
     for i in range(len(dane)):
-        dane2= re.findall(example2,dane[i])
+        dane2= re.findall(example2,dane[i])   #korzystam z regexa, żeby uzyskać liczby i wykonuje funkcje mul()
         wynik = wynik + (int(dane2[0])*int(dane2[1]))
-
 
 
 
