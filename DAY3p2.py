529a6c4b141a561bbf62307625f10da9da631991
Franciszek Żółtogórski <152714905+Mangutek@users.noreply.github.com>
2024-12-04 14:02:05 +0100

    Update DAY3p2.py

--- DAY3p2.py
+++ DAY3p2.py
@@ -1,19 +1,19 @@
-import re
-file = open("C:\\Users\\Franek\\Desktop\\AOC\\DAY3\\database3.txt")
+import re #importuje REGULAR EXPRESION
+file = open("C:\\Users\\Franek\\Desktop\\AOC\\DAY3\\database3.txt") #wczytuje plik
 tekst = file.readlines()
 data = []
 wynik =0
 enable = 1
 
-for linia in tekst:
+for linia in tekst:  #wczytuje dane do tabeli
     data.append(linia)
 
-example = "mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)"
+example = "mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)"  #dodałem do wzoru do() i don't() (w tabeli będą się znajdować pomiędzy mul())
 example2 = '[0-9]+'
 
 
 
-for n in range(len(data)):
+for n in range(len(data)):  #w zależności od do() i don't() zmieniam wartość enable i albo wykonuje mul() albo nie
     dane = re.findall(example, data[n])
     print(dane)
     for i in range(len(dane)):
