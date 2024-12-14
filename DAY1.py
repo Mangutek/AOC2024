7200df276f0369798861afb832a9cab40a6899c2
Franciszek Żółtogórski <152714905+Mangutek@users.noreply.github.com>
2024-12-14 22:01:37 +0100

    Update DAY1.py

--- DAY1.py
+++ DAY1.py
@@ -1,30 +1,30 @@
-file = open("C:\\Users\\Admin\\Desktop\\AOC\\DAY1\\database1.txt") #wczytuje plik
-tekst = file.readlines()
-txt = []
-LEWA = []
-PRAWA = []
-wynik = 0
+#wczytuje plik
+text = open("C:\\Users\\Admin\\Desktop\\AOC\\DAY1\\database1.txt").readlines()
+data = []
+left = []
+right = []
+score = 0
 
-for line in tekst:        #wprowadzam dane do tabeli
+for line in text:        #wprowadzam dane do tabeli
     roz1 = line.split('   ')
     for el in roz1:
         el2 = el.strip()
-        txt.append(el2)
+        data.append(el2)
 
 i = 2
 for el in txt:        #dziele na dwie tabele
     if i%2 == 0:
-        LEWA.append(el)
+        left.append(el)
     else:
-        PRAWA.append(el)
+        right.append(el)
     i = i+1
 
 PRAWA.sort() #sortuje
 LEWA.sort()
 
-for i in range(len(LEWA)):  #sumuje róznice pomiędzy nimi
+for i in range(len(left)):  #sumuje róznice pomiędzy nimi
     p = 0
-    p = int(LEWA[i]) - int(PRAWA[i])
+    p = int(left[i]) - int(right[i])
     p2 = abs(p)
-    wynik = wynik + p2
-print(wynik)
+    score = score + p2
+print(score)
